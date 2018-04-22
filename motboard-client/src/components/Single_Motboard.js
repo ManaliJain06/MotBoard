b422d428468ff00a7eedb9804821dc15e7905749
manvithac <manvitha.challagundla@gmail.com>
2018-04-21 18:40:31 -0700

    new mot_board

---
+++ motboard-client/src/components/Single_Motboard.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Route, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+class Single_Motboard extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="row justify-content-center">
+                    
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Single_Motboard);
