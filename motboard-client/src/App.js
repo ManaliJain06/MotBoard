c1c959ea2dd243069b97b77458176c64f8eac337
manvithac <manvitha.challagundla@gmail.com>
2018-05-01 10:42:49 -0700

    Pagination - partial

--- motboard-client/src/App.js
+++ motboard-client/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Homepage from './components/Homepage';
+import Homepage from './components/Homepage'
+import ErrorBoundary from './components/ErrorBoundaryPage';
+
 class App extends Component {
   render() {
     return (
           <div>
+              <ErrorBoundary>
               <MuiThemeProvider>
               <BrowserRouter>
                   <Homepage/>
               </BrowserRouter>
               </MuiThemeProvider>
+              </ErrorBoundary>
           </div>
     );
   }
