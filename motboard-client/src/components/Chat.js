42a99848187db009fbabbbafc9e23aa81db38bd5
manvithac <manvitha.challagundla@gmail.com>
2018-05-10 10:37:00 -0700

    voice and chat bot working

--- motboard-client/src/components/Chat.js
+++ motboard-client/src/components/Chat.js
@@ -15,34 +15,130 @@ const style = {
 const theme = {
     fontFamily: 'inherit',
     headerBgColor: '#fff176',
-    headerFontColor: '#fff',
+    headerFontColor: 'black',
     headerFontSize: '10px',
-    botBubbleColor: '#fff176',
+    botBubbleColor: '#BA68C8',
     botFontColor: 'black',
     userBubbleColor: '#fff',
     userFontColor: 'black',
     footerFontColor:'black'
 };
-
+var name='';
 let steps = [
     {
         id: '1',
-        message: 'What is your favourite color?',
+        message: 'Hi there! Welcome to Motboard.',
         trigger: '2',
     },
     {
         id: '2',
-        user:true,
-        trigger:'4'
+        message: 'Before you start, guess what my Favorite color is?',
+        trigger: '3',
     },
     {
-        id:'4',
-        message:'done'
-    }
+        id: '3',
+        options: [
+            { value: 1, label: 'Orange', trigger: '4' },
+            { value: 2, label: 'Yellow', trigger: '5' },
+            { value: 3, label: 'Blue', trigger: '4' },
+        ],
+    },
+    {
+        id: '4',
+        message: 'Wrong answer, try again.',
+        trigger: '3',
+    },
+    {
+        id: '5',
+        message: 'Awesome! You are a telepath!',
+        trigger: '6',
+    },
+    {
+        id: '6',
+        message: 'Oh! I forgot to ask your name. and you are...?',
+        trigger: '7',
+    },
+    {
+        id: '7',
+        user: true,
+        validator: (value) => {
+            if (!isNaN(value)) {
+                return 'Enter word. I am not so good at numbers you kno. ugh!';
+            }
+            else{
+                name = value ;
+            }
+            return true;
+        },
+        trigger: '8',
+    },
+    {
+        id: '8',
+        message: 'Hey there {previousValue}. I am here to help you suggest colors. Choose one below and I will suggest what color could be a good match.',
+        trigger: '9',
+    },
+    {
+        id: '9',
+        options: [
+            { value: 1, label: 'Orange', trigger: '10' },
+            { value: 2, label: 'Yellow', trigger: '12' },
+            { value: 3, label: 'Green', trigger: '13' },
+        ],
+    },
+    {
+        id: '10',
+        message: 'Hmm.. I think Orange looks good with blue',
+        trigger:'11'
+    },
+    {
+        id: '11',
+        component: (
+            <div style={{'overflow':'hidden'}}>
+                <div style={{'background-color':'#29B6F6','height':'50px','width':'100vw','text-align':'center'}}>#29B6F6</div>
+                <div style={{'background-color':'#B3E5FC','height':'50px','width':'100vw','text-align':'center'}}>#B3E5FC</div>
+                <div style={{'background-color':'#0277BD','height':'50px','width':'100vw','text-align':'center'}}>#0277BD</div>
+                <div style={{'background-color':'#FF8A65','height':'50px','width':'100vw','text-align':'center'}}>#FF8A65</div>
+                <div style={{'background-color':'#FF9E80','height':'50px','width':'100vw','text-align':'center'}}>#FF9E80</div>
+                <div style={{'background-color':'#FF7043','height':'50px','width':'100vw','text-align':'center'}}>#FF7043</div>
+            </div>
+        ),
+        trigger: '9',
+    },
+    {
+        id: '12',
+        component: (
+            <div style={{'overflow':'hidden'}}>
+                <div style={{'background-color':'#FFFDE7','height':'50px','width':'100vw','text-align':'center'}}>#FFFDE7</div>
+                <div style={{'background-color':'#FFFF8D','height':'50px','width':'100vw','text-align':'center'}}>#FFFF8D</div>
+                <div style={{'background-color':'#FFF176','height':'50px','width':'100vw','text-align':'center'}}>#FFF176</div>
+                <div style={{'background-color':'#B39DDB','height':'50px','width':'100vw','text-align':'center'}}>#B39DDB</div>
+                <div style={{'background-color':'#5E35B1','height':'50px','width':'100vw','text-align':'center'}}>#5E35B1</div>
+                <div style={{'background-color':'#D1C4E9','height':'50px','width':'100vw','text-align':'center'}}>#D1C4E9</div>
+            </div>
+        ),
+        trigger: '9',
+    },
+    {
+        id: '13',
+        component: (
+            <div style={{'overflow':'hidden'}}>
+                <div style={{'background-color':'#C5E1A5','height':'50px','width':'100vw','text-align':'center'}}>#C5E1A5</div>
+                <div style={{'background-color':'#7CB342','height':'50px','width':'100vw','text-align':'center'}}>#7CB342</div>
+                <div style={{'background-color':'#558B2F','height':'50px','width':'100vw','text-align':'center'}}>#558B2F</div>
+                <div style={{'background-color':'#D7CCC8','height':'50px','width':'100vw','text-align':'center'}}>#D7CCC8</div>
+                <div style={{'background-color':'#A1887F','height':'50px','width':'100vw','text-align':'center'}}>#A1887F</div>
+                <div style={{'background-color':'#6D4C41','height':'50px','width':'100vw','text-align':'center'}}>#6D4C41</div>
+            </div>
+        ),
+        trigger: '9',
+    },
 ];
 class Chat extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
+        this.state={
+            name: '',
+        }
     }
 
     state = {
@@ -66,13 +162,16 @@ class Chat extends Component {
                 {this.state.chatOpen ?
                     <div className="chat-bg">
                         <ThemeProvider theme={theme}>
-                            <ChatBot steps={steps} headerComponent={
-                                <div style={{'background-color':'#fff176'}}>
-                                    <Close onClick={() => {
-                                        this.closechat()
-                                    }}   />
-                                </div>
-                            } />
+                            <ChatBot steps={steps}
+                                     headerTitle="Speech Recognition"
+                                     recognitionEnable={true}
+                                     headerComponent={
+                                         <div style={{'background-color':'#BA68C8'}}>
+                                             <Close onClick={() => {
+                                                 this.closechat()
+                                             }}   />
+                                         </div>
+                                     } />
 
                         </ThemeProvider>
                     </div>
@@ -80,7 +179,7 @@ class Chat extends Component {
                     <div onClick={() => {
                         this.openchat()
                     }}>
-                        <FloatingActionButton backgroundColor={'#fff176'} style={style}>
+                        <FloatingActionButton backgroundColor={'#BA68C8'} style={style}>
                             <Chaticon/>
                         </FloatingActionButton>
                     </div>
--- motboard-client/src/components/ColorsGenerator.js
+++ motboard-client/src/components/ColorsGenerator.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 import {Route, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import '../css/colorgen.css';
-import VoiceTest from "./VoiceTest";
 import Artyom from 'artyom.js';
 const artyom = new Artyom();
+
+// Start the commands !
+
 
 class ColorsGenerator extends Component{
     constructor(props){
@@ -19,7 +21,97 @@ class ColorsGenerator extends Component{
         }
     }
     componentWillMount(){
+        artyom.initialize({
+            lang: "en-US", // GreatBritain english
+            continuous: true, // Listen forever
+            soundex: true,// Use the soundex algorithm to increase accuracy
+            debug: true, // Show messages in the console
+            executionKeyword: "Do it now",
+            listen: true, // Start to listen commands !
+
+            // If providen, you can only trigger a command if you say its name
+            // e.g to trigger Good Morning, you need to say "Jarvis Good Morning"
+            name: "Rainbow"
+        }).then(() => {
+            console.log("Artyom has been succesfully initialized");
+        }).catch((err) => {
+            console.error("Artyom couldn't be initialized: ", err);
+        });
+
+//artyom.say("Hello. My name is Rainbow ! I can help you choose colors");
+
+        artyom.on(['Good morning','Good afternoon']).then((i) => {
+            switch (i) {
+                case 0:
+                    artyom.say("Good morning, how are you?");
+                    break;
+                case 1:
+                    artyom.say("Good afternoon, how are you?");
+                    break;
+            }
+        });
+
+// Smart command (Short code artisan way), set the second parameter of .on to true
+        artyom.on(['Repeat after me *'] , true).then((i,wildcard) => {
+            artyom.say("You've said : " + wildcard);
+        });
+
+// or add some commandsDemostrations in the normal way
+
         artyom.say("Hello. My name is Rainbow ! I can help you choose colors");
+        artyom.addCommands([
+            {
+                indexes: ['Hello','Hi','is someone there'],
+                action: (i) => {
+                    artyom.say("Hello, it's me");
+                }
+            },
+            {
+                indexes: ['Repeat after me *'],
+                smart:true,
+                action: (i,wildcard) => {
+                    artyom.say("You've said : "+ wildcard);
+                }
+            },
+            // The smart commands support regular expressions
+            {
+                indexes: [/Good Morning/i],
+                smart:true,
+                action: (i,wildcard) => {
+                    artyom.say("You've said : "+ wildcard);
+                }
+            },
+            {
+                indexes: ['shut down yourself'],
+                action: (i,wildcard) => {
+                    artyom.fatality().then(() => {
+                        console.log("Artyom succesfully stopped");
+                    });
+                }
+            },
+            {
+                indexes: ['Magic'],
+                action: (i,wildcard) => {
+                    this.generateColors();
+                        var currentTime = new Date().getTime();
+                        while (currentTime + 500 >= new Date().getTime()) {
+                        }
+                    this.generateColors();
+                    var currentTime = new Date().getTime();
+                    while (currentTime + 500 >= new Date().getTime()) {
+                    }
+                    this.generateColors();
+                    var currentTime = new Date().getTime();
+                    while (currentTime + 500 >= new Date().getTime()) {
+                    }
+                    this.generateColors();
+                    artyom.fatality().then(() => {
+                        artyom.say("There you go!!");
+                    });
+                }
+            },
+        ]);
+
     }
     generateColors = () =>{
         console.log('Random color generator');
@@ -66,7 +158,6 @@ class ColorsGenerator extends Component{
                 <div className="col-md-2 onecolumn Questrial" style={{'background-color': this.state.RandomColor5}}>{this.state.RandomColor5}</div>
                 <div className="col-md-2 onecolumn Questrial" style={{'background-color': this.state.RandomColor6}}>{this.state.RandomColor6}</div>
             </div>
-                <VoiceTest/>
             </div>
         )
     }
